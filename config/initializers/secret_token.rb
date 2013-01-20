971e2dc1bba9e4b376b233ad243d4ab3dc386fff
4cubits <aglazer@fourcubits.com>
2013-01-20 17:07:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carsapp::Application.config.secret_token = 'f0c57723a393a8303acd89b5bcbd31a2ff9ca3744daaddab05bd5de36fb916ed02856e784d53062f219bb4a923deebe652cfa965e816af670afea79537651fe5'
